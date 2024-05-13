29cc17c6d3e1594998bec61a54bcc160a985a95a
etsuee <148324929+etsuee@users.noreply.github.com>
2024-05-13 13:02:31 +0200

    ajout ex07

---
+++ ex07/ex07.py
@@ -0,0 +1,14 @@
+num = int(input("Entrez un numéro: "))
+
+somme = 0
+
+temp = num
+while temp > 0:
+   digit = temp % 10
+   somme += digit ** 3
+   temp //= 10
+
+if num == somme:
+   print(num,"est un numéro d'Armstrong")
+else:
+   print(num,"n'est pas un numéro Armstrong")
